9c95b8b4c94aaa8830fef454b92cc25561c26e79
Vivek Kumar <anemail2vivek@gmail.com>
2024-08-15 00:06:06 +0530

    script correction

--- script.py
+++ script.py
@@ -3,5 +3,5 @@ from datetime import datetime
 def now():
   return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-if __name__==__main__:
+if __name__=="__main__":
   print(f'Hi, Current time is {now()}')
